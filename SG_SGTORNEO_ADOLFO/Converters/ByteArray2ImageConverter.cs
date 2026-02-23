35abb361a1989f60a2cc714dc3316a3f170f4e65
Adolfo <adomorang@gmail.com>
2026-02-23 21:38:45 +0100

    creación del converter vacío

---
+++ SG_SGTORNEO_ADOLFO/Converters/ByteArray2ImageConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SG_SGTORNEO_ADOLFO.Converters
+{
+    public class ByteArray2ImageConverter
+    {
+    }
+}
