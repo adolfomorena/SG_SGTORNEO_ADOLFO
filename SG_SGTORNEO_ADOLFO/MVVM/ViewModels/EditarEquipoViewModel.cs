bb1605da1c18971e0b550f1df8238e66f93146f5
Adolfo <adomorang@gmail.com>
2026-02-24 04:44:48 +0100

    funcionalidad añadir  equipo

--- SG_SGTORNEO_ADOLFO/MVVM/ViewModels/EditarEquipoViewModel.cs
+++ SG_SGTORNEO_ADOLFO/MVVM/ViewModels/EditarEquipoViewModel.cs
@@ -1,12 +1,64 @@
-﻿using System;
+﻿using PropertyChanged;
+using SG_SGTORNEO_ADOLFO.MVVM.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace SG_SGTORNEO_ADOLFO.MVVM.ViewModels
 {
-    class EditarEquipoViewModel
+    [AddINotifyPropertyChangedInterface]
+    public class EditarEquipoViewModel
     {
+        public string Nombre { get; set; }
+        public Equipo EquipoActual { get; set; }
+
+        public ICommand GuardarCommand { get; set; }
+        public ICommand CancelarCommand { get; set; }
+
+        public EditarEquipoViewModel(Equipo equipo = null)
+        {
+            EquipoActual = equipo;
+            if (equipo != null)
+            {
+                Nombre = equipo.Nombre;
+            }
+
+            GuardarCommand = new Command(Guardar, PuedeGuardar);
+            CancelarCommand = new Command(Cancelar);
+        }
+
+        private bool PuedeGuardar()
+        {
+            if (string.IsNullOrWhiteSpace(Nombre))
+                return false;
+
+            return true;
+        }
+
+        public void NotificarCambio()
+        {
+            ((Command)GuardarCommand).ChangeCanExecute();
+        }
+
+        private async void Guardar()
+        {
+            if (EquipoActual == null)
+            {
+                EquipoActual = new Equipo();
+            }
+            EquipoActual.Nombre = Nombre;
+
+            App.EquiposRepo.SaveItem(EquipoActual);
+
+            await App.Current.MainPage.Navigation.PopAsync();
+        }
+
+        private async void Cancelar()
+        {
+            await App.Current.MainPage.Navigation.PopAsync();
+        }
     }
 }
--- SG_SGTORNEO_ADOLFO/MVVM/Views/EditarEquipoView.xaml.cs
+++ SG_SGTORNEO_ADOLFO/MVVM/Views/EditarEquipoView.xaml.cs
@@ -1,9 +1,18 @@
+using SG_SGTORNEO_ADOLFO.MVVM.Models;
+using SG_SGTORNEO_ADOLFO.MVVM.ViewModels;
+
 namespace SG_SGTORNEO_ADOLFO.MVVM.Views;
 
 public partial class EditarEquipoView : ContentPage
 {
-	public EditarEquipoView()
+	public EditarEquipoView(Equipo equipo = null)
 	{
 		InitializeComponent();
-	}
+		BindingContext = new EditarEquipoViewModel(equipo);
+    }
+
+    private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        ((EditarEquipoViewModel)BindingContext).NotificarCambio();
+    }
 }
