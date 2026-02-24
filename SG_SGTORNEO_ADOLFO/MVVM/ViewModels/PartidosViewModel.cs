fc2af73e1b43c2168b7b961f58246bdbda5bbb5d
Adolfo <adomorang@gmail.com>
2026-02-24 02:28:24 +0100

    vista de la lista de partidos

--- SG_SGTORNEO_ADOLFO/MVVM/ViewModels/PartidosViewModel.cs
+++ SG_SGTORNEO_ADOLFO/MVVM/ViewModels/PartidosViewModel.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using PropertyChanged;
+using SG_SGTORNEO_ADOLFO.MVVM.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SG_SGTORNEO_ADOLFO.MVVM.ViewModels
 {
+    [AddINotifyPropertyChangedInterface]
     public class PartidosViewModel
     {
+        public ObservableCollection<Partido> Partidos { get; set; } = new();
     }
 }
