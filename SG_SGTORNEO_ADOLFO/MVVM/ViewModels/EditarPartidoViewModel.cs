5633a5b5fe68a3eb4f75c039bb969d9d73eeebeb
Adolfo <adomorang@gmail.com>
2026-02-24 02:49:30 +0100

    solución ciertos errores con la vista de partidos

--- SG_SGTORNEO_ADOLFO/MVVM/ViewModels/EditarPartidoViewModel.cs
+++ SG_SGTORNEO_ADOLFO/MVVM/ViewModels/EditarPartidoViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SG_SGTORNEO_ADOLFO.MVVM.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace SG_SGTORNEO_ADOLFO.MVVM.ViewModels
 {
     public class EditarPartidoViewModel
     {
+        public EditarPartidoViewModel(Partido partido = null)
+        {
+
+        }
     }
 }
--- SG_SGTORNEO_ADOLFO/MVVM/Views/EditarPartidoView.xaml.cs
+++ SG_SGTORNEO_ADOLFO/MVVM/Views/EditarPartidoView.xaml.cs
@@ -1,12 +1,13 @@
+using SG_SGTORNEO_ADOLFO.MVVM.Models;
 using SG_SGTORNEO_ADOLFO.MVVM.ViewModels;
 
 namespace SG_SGTORNEO_ADOLFO.MVVM.Views;
 
 public partial class EditarPartidoView : ContentPage
 {
-	public EditarPartidoView()
+	public EditarPartidoView(Partido partido = null)
 	{
 		InitializeComponent();
-		BindingContext = new EditarPartidoViewModel();
+		BindingContext = new EditarPartidoViewModel(partido);
     }
 }
