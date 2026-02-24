06659c11bf32b43ea722816d9d5453aa0fe57fa4
Adolfo <adomorang@gmail.com>
2026-02-24 04:32:49 +0100

    funcionalidad de los botonnes y creacion pagina añadir equipo

--- SG_SGTORNEO_ADOLFO/MVVM/ViewModels/EquiposViewModel.cs
+++ SG_SGTORNEO_ADOLFO/MVVM/ViewModels/EquiposViewModel.cs
@@ -1,12 +1,48 @@
-﻿using System;
+﻿using PropertyChanged;
+using SG_SGTORNEO_ADOLFO.MVVM.Models;
+using SG_SGTORNEO_ADOLFO.MVVM.Views;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace SG_SGTORNEO_ADOLFO.MVVM.ViewModels
 {
+    [AddINotifyPropertyChangedInterface]
     public class EquiposViewModel
     {
+        public ObservableCollection<Equipo> Equipos { get; set; }
+
+        public ICommand NuevoEquipoCommand { get; set; }
+        public ICommand EditarEquipoCommand { get; set; }
+        public ICommand EliminarEquipoCommand { get; set; }
+
+        public EquiposViewModel()
+        {
+            Equipos = new ObservableCollection<Equipo>(App.EquiposRepo.GetItems());
+
+            NuevoEquipoCommand = new Command(NuevoEquipo);
+            EditarEquipoCommand = new Command<Equipo>(EditarEquipo);
+            EliminarEquipoCommand = new Command<Equipo>(EliminarEquipo);
+        }
+
+        private void NuevoEquipo()
+        {
+            App.Current.MainPage.Navigation.PushAsync(new EditarEquipoView());
+        }
+
+        private void EditarEquipo(Equipo equipo)
+        {
+            App.Current.MainPage.Navigation.PushAsync(new EditarEquipoView(equipo));
+        }
+
+        private void EliminarEquipo(Equipo equipo)
+        {
+            App.EquiposRepo.DeleteItem(equipo);
+            Equipos.Remove(equipo);
+        }
     }
 }
